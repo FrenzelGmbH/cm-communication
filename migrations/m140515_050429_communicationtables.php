b3d9e4d430825dada32dce14a3ad909187a575db
Philipp Frenzel <philipp@frenzel.net>
2014-05-19 21:05:23 +0200

    fis

--- migrations/m140515_050429_communicationtables.php
+++ migrations/m140515_050429_communicationtables.php
@@ -15,22 +15,22 @@ class m140515_050429_communicationtables extends \yii\db\Migration
 	{
     
     switch (Yii::$app->db->driverName) {
-        case 'mysql':
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
-            break;
-        case 'pgsql':
-            $tableOptions = null;
-            break;
-        default:
-            throw new RuntimeException('Your database is not supported!');
+      case 'mysql':
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        break;
+      case 'pgsql':
+        $tableOptions = null;
+        break;
+      default:
+        throw new RuntimeException('Your database is not supported!');
     }
 
 		$this->createTable('{{%communication}}',[
-      'id'     => Schema::TYPE_PK,
-      'mobile' => Schema::TYPE_STRING .'(200)',
-      'phone'  => Schema::TYPE_STRING .'(200)',
-      'fax'    => Schema::TYPE_STRING .'(200)',
-      'email'  => Schema::TYPE_STRING .'(200)',
+      'id'                => Schema::TYPE_PK,
+      'mobile'            => Schema::TYPE_STRING .'(200)',
+      'phone'             => Schema::TYPE_STRING .'(200)',
+      'fax'               => Schema::TYPE_STRING .'(200)',
+      'email'             => Schema::TYPE_STRING .'(200)',
       //possible reference to user
       'user_id'           => Schema::TYPE_INTEGER.' NULL',
       //module fields
@@ -49,8 +49,18 @@ class m140515_050429_communicationtables extends \yii\db\Migration
     ],$tableOptions);
 
     $this->createTable('{{%communication_type}}',[
-        'id'            => Schema::TYPE_PK,
-        'name'          => Schema::TYPE_STRING .'(100)',
+      'id'                => Schema::TYPE_PK,
+      'name'              => Schema::TYPE_STRING .'(100)',
+      //possible reference to user
+      'user_id'           => Schema::TYPE_INTEGER.' NULL',
+      //interface fields
+      'system_key'        => Schema::TYPE_STRING .'(100)',
+      'system_name'       => Schema::TYPE_STRING .'(100)',
+      'system_upate'      => Schema::TYPE_INTEGER.' DEFAULT NULL',
+      // timestamps
+      'created_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
+      'updated_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
+      'deleted_at'        => Schema::TYPE_INTEGER . ' DEFAULT NULL'
     ],$tableOptions);
 
     $this->addForeignKey('fk_communication_communication_type', '{{%communication}}', 'communication_type_id', '{{%communication_type}}', 'id', 'CASCADE', 'RESTRICT');
