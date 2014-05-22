a4d48425ea42234cef1bd6c925bddffff245ff4b
Philipp Frenzel <philipp@frenzel.net>
2014-05-22 22:35:01 +0200

    fix

--- views/communication-type/_form.php
+++ views/communication-type/_form.php
@@ -14,21 +14,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
-    <?= $form->field($model, 'system_upate')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'deleted_at')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'system_key')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'system_name')->textInput(['maxlength' => 100]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
